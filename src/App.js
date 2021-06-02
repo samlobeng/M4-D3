2f6103270dbb647764635431aef71b5ca90e2c0b
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-06-02 15:54:55 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MySign from './components/WarningSign'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyBadge from './components/MyBadge';
 
 function App() {
-  return(
+  return(<>
     <MySign   warning={"WARNING MFS!"} />
-  )
+    <MyBadge title = "Number of Books" color = "Secondary"/>
+ </> )
  
 }
 
---
+++ src/components/MyBadge.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button,Badge } from "react-bootstrap";
+
+export default function MyBadge(props) {
+  return (
+    <Button variant="primary">
+      {props.title} <Badge bg={props.color}>9</Badge>
+      <span className="visually-hidden">unread messages</span>
+    </Button>
+  );
+}
