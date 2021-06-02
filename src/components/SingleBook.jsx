fcc7e2b67f5a96c3fde4b374f7ea878873e28124
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-06-02 13:56:11 +0000

    Update Carl Branch
    Exercise 3 Complete

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,26 @@
 import  { Component } from 'react'
-import fantasy from '../fantasy.json'
-class SingleBook extends Component {
+import {Col,Row,Card} from 'react-bootstrap'
+class Book extends Component {
 
     render() {
         return (
         <>
+        <Row xs={1} md={2} className="g-4">
+  {
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={this.props.data.img} />
+        <Card.Body>
+          <Card.Title>{this.props.data.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </Col>
+  }
+</Row>
 
         </>
         )
     }
 }
 
-export default SingleBook
+export default Book
